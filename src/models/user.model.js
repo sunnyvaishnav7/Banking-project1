22aef6aca5a7e8b878381dfe4302693dd0d32a7e
sunnyvaishnav7 <www.sunnyvaishnavh77@gmail.com>
2026-09-25 11:15:09 +0530

    userModel created

---
+++ src/models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Email is require"],
+        trim: true,
+        lowercase: true,
+        match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required for creating an account"]
+    },
+    password: {
+        type: String,
+        required: [true, "Password is require for account"],
+        minlength: [6, "password should be contain more then 6 characther"],
+        select: false
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) {
+        return next()
+    }
+
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+
+    return next()
+
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel;
