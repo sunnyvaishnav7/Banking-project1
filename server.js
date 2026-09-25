d15c61d806be6c5ceccb757c0e9f6ba510b2b945
sunnyvaishnav7 <www.sunnyvaishnavh77@gmail.com>
2026-09-25 14:53:00 +0530

    Register API created

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require("dotenv").config()
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-connectToDB();
+connectDB();
 
-app.listen(3000 ,() =>{
+app.listen(3000, () => {
     console.log("server is running on port 3000");
 })
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const authController = require("../controllers/auth.controllers")
+
+const router = express.Router()
+
+
+
+router.post("/register", authController.userRegisterController)
+
+module.exports = router 
