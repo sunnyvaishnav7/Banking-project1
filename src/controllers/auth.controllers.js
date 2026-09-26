6e80c55e5d1f9ec2c4d55c11aa7891c1a4269584
sunnyvaishnav7 <www.sunnyvaishnavh77@gmail.com>
2026-09-26 14:27:08 +0530

    nodemailer  and welcome mail setup added

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const emailService = require("../services/email.service")
 
 // user register authController
 // POST /api/auth/register
@@ -36,6 +37,8 @@ async function userRegisterController(req, res) {
         },
         token
     })
+
+    await emailService.sendRegistrationEmail(user.email , user.name);
 
 }
 
